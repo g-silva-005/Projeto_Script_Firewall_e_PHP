3f47e2af27d572aa0cd5b5ce93abf6ce73bf4ec9
g-silva-005 <1999goncalo.gs@gmail.com>
2026-02-05 10:55:34 +0000

    Add files via upload

---
+++ firewall_php.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+cat << END
+
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+FFFFFFFFFFFFFFFFFFFFFFIIIIIIIIIIRRRRRRRRRRRRRRRRR   EEEEEEEEEEEEEEEEEEEEEEWWWWWWWW                           WWWWWWWW   AAA               LLLLLLLLLLL             LLLLLLLLLLL             
+F::::::::::::::::::::FI::::::::IR::::::::::::::::R  E::::::::::::::::::::EW::::::W                           W::::::W  A:::A              L:::::::::L             L:::::::::L             
+F::::::::::::::::::::FI::::::::IR::::::RRRRRR:::::R E::::::::::::::::::::EW::::::W                           W::::::W A:::::A             L:::::::::L             L:::::::::L             
+FF::::::FFFFFFFFF::::FII::::::IIRR:::::R     R:::::REE::::::EEEEEEEEE::::EW::::::W                           W::::::WA:::::::A            LL:::::::LL             LL:::::::LL             
+  F:::::F       FFFFFF  I::::I    R::::R     R:::::R  E:::::E       EEEEEE W:::::W           WWWWW           W:::::WA:::::::::A             L:::::L                 L:::::L               
+  F:::::F               I::::I    R::::R     R:::::R  E:::::E               W:::::W         W:::::W         W:::::WA:::::A:::::A            L:::::L                 L:::::L               
+  F::::::FFFFFFFFFF     I::::I    R::::RRRRRR:::::R   E::::::EEEEEEEEEE      W:::::W       W:::::::W       W:::::WA:::::A A:::::A           L:::::L                 L:::::L               
+  F:::::::::::::::F     I::::I    R:::::::::::::RR    E:::::::::::::::E       W:::::W     W:::::::::W     W:::::WA:::::A   A:::::A          L:::::L                 L:::::L               
+  F:::::::::::::::F     I::::I    R::::RRRRRR:::::R   E:::::::::::::::E        W:::::W   W:::::W:::::W   W:::::WA:::::A     A:::::A         L:::::L                 L:::::L               
+  F::::::FFFFFFFFFF     I::::I    R::::R     R:::::R  E::::::EEEEEEEEEE         W:::::W W:::::W W:::::W W:::::WA:::::AAAAAAAAA:::::A        L:::::L                 L:::::L               
+  F:::::F               I::::I    R::::R     R:::::R  E:::::E                    W:::::W:::::W   W:::::W:::::WA:::::::::::::::::::::A       L:::::L                 L:::::L               
+  F:::::F               I::::I    R::::R     R:::::R  E:::::E       EEEEEE        W:::::::::W     W:::::::::WA:::::AAAAAAAAAAAAA:::::A      L:::::L         LLLLLL  L:::::L         LLLLLL
+FF:::::::FF           II::::::IIRR:::::R     R:::::REE::::::EEEEEEEE:::::E         W:::::::W       W:::::::WA:::::A             A:::::A   LL:::::::LLLLLLLLL:::::LLL:::::::LLLLLLLLL:::::L
+F::::::::FF           I::::::::IR::::::R     R:::::RE::::::::::::::::::::E          W:::::W         W:::::WA:::::A               A:::::A  L::::::::::::::::::::::LL::::::::::::::::::::::L
+F::::::::FF           I::::::::IR::::::R     R:::::RE::::::::::::::::::::E           W:::W           W:::WA:::::A                 A:::::A L::::::::::::::::::::::LL::::::::::::::::::::::L
+FFFFFFFFFFF           IIIIIIIIIIRRRRRRRR     RRRRRRREEEEEEEEEEEEEEEEEEEEEE            WWW             WWWAAAAAAA                   AAAAAAALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+EEEEEEEEEEEEEEEEEEEEEE                                                                                                                                                                    
+E::::::::::::::::::::E                                                                                                                                                                    
+E::::::::::::::::::::E                                                                                                                                                                    
+EE::::::EEEEEEEEE::::E                                                                                                                                                                    
+  E:::::E       EEEEEE                                                                                                                                                                    
+  E:::::E                                                                                                                                                                                 
+  E::::::EEEEEEEEEE                                                                                                                                                                       
+  E:::::::::::::::E                                                                                                                                                                       
+  E:::::::::::::::E                                                                                                                                                                       
+  E::::::EEEEEEEEEE                                                                                                                                                                       
+  E:::::E                                                                                                                                                                                 
+  E:::::E       EEEEEE                                                                                                                                                                    
+EE::::::EEEEEEEE:::::E                                                                                                                                                                    
+E::::::::::::::::::::E                                                                                                                                                                    
+E::::::::::::::::::::E                                                                                                                                                                    
+EEEEEEEEEEEEEEEEEEEEEE                                                                                                                                                                    
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+PPPPPPPPPPPPPPPPP   HHHHHHHHH     HHHHHHHHHPPPPPPPPPPPPPPPPP                                                                                                                              
+P::::::::::::::::P  H:::::::H     H:::::::HP::::::::::::::::P                                                                                                                             
+P::::::PPPPPP:::::P H:::::::H     H:::::::HP::::::PPPPPP:::::P                                                                                                                            
+PP:::::P     P:::::PHH::::::H     H::::::HHPP:::::P     P:::::P                                                                                                                           
+  P::::P     P:::::P  H:::::H     H:::::H    P::::P     P:::::P                                                                                                                           
+  P::::P     P:::::P  H:::::H     H:::::H    P::::P     P:::::P                                                                                                                           
+  P::::PPPPPP:::::P   H::::::HHHHH::::::H    P::::PPPPPP:::::P                                                                                                                            
+  P:::::::::::::PP    H:::::::::::::::::H    P:::::::::::::PP                                                                                                                             
+  P::::PPPPPPPPP      H:::::::::::::::::H    P::::PPPPPPPPP                                                                                                                               
+  P::::P              H::::::HHHHH::::::H    P::::P                                                                                                                                       
+  P::::P              H:::::H     H:::::H    P::::P                                                                                                                                       
+  P::::P              H:::::H     H:::::H    P::::P                                                                                                                                       
+PP::::::PP          HH::::::H     H::::::HHPP::::::PP                                                                                                                                     
+P::::::::P          H:::::::H     H:::::::HP::::::::P                                                                                                                                     
+P::::::::P          H:::::::H     H:::::::HP::::::::P                                                                                                                                     
+PPPPPPPPPP          HHHHHHHHH     HHHHHHHHHPPPPPPPPPP                                                                                                                                     
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+                                                                                                                                                                                          
+END
+
+#pequeno menu de introducao
+cat << END
+Escolha, por favor, uma das seguintes opcoes:
+
+1: Ativar/Desativar portas de firewall (22,80,443)
+2: Instalar o PHP e o modulos necessários
+3: Testar o funcionamento PHP (Verificar as cenas)
+4: Fechar o programa
+
+END
+
+while true; do
+	read -p ": " rigby
+	case $rigby in
+		1)
+		printf "/n"
+		cat << END
+Escolha, por favor, uma das seguintes opções:
+1: Ativar porta/s 
+2: Desativar porta/s
+3: Sair 
+END
+		while true; do 
+			read -p ": " w
+			case $w in
+				1)
+				read -p "Ativar todas ou somente alguma porta em especifíco? Escreva todas (para ativar todas) ou alguma (para ativar apenas alguma porta em especifíco): " porta
+				if [ $porta = todas ]; then
+					echo "----- A ativar portas de firewall ------"
+					sudo firewall-cmd --permanent --add-port=22/tcp
+					sudo firewall-cmd --permanent --add-port=80/tcp
+					sudo firewall-cmd --permanent --add-port=443/tcp
+					echo "----- Todas as Portas ativadas com sucesso -----"
+					break
+				
+				elif [ $porta = alguma ]; then
+					printf "/n"
+					cat << END
+Selecione a porta que deseja ativar:
+1: Porta 22
+2: Porta 80
+3: Porta 443
+END
+					while true; do
+						read -p ": " L
+						case $L in 
+						1)
+						echo "----- A ativar porta de firewall ------"
+						sudo firewall-cmd --permanent --add-port=22/tcp
+						echo "----- Todas as Portas ativadas com sucesso -----"
+						break
+						;;
+						2)
+						echo "----- A ativar porta de firewall ------"
+						sudo firewall-cmd --permanent --add-port=80/tcp
+						echo "----- Todas as Porta ativadas com sucesso -----"
+						break
+						;;
+						3)
+						echo "----- A ativar porta de firewall ------"
+						sudo firewall-cmd --permanent --add-port=443/tcp
+						
+						echo "----- Todas as Porta ativadas com sucesso -----"
+						break
+						;;
+						*)
+						echo "Escolha inválida, escreva todas ou alguma."
+						;;
+						esac
+					done 
+				else
+					echo " A sair do programa..."
+					break
+					exit 0
+				fi
+				
+				;;
+				
+				2)
+				read -p "Por segurança, escreva o nome da porta que quer desativar (ssh, http, https): " desativar
+				if [ $desativar = ssh ]; then
+				echo "----- A desativar porta de firewall ------"
+				sudo firewall-cmd --permanent --remove-port=22/tcp
+				echo "----- A porta selecionada foi desativada com sucesso!  -----"
+				elif [ $desativar = http ]; then
+				echo "----- A desativar porta de firewall ------"
+				sudo firewall-cmd --permanent --remove-port=80/tcp
+				echo "----- A porta selecionada foi desativada com sucesso!  -----"
+				elif [ $desativar = https ]; then
+				echo "----- A desativar porta de firewall ------"
+				sudo firewall-cmd --permanent --remove-port=443/tcp 
+				echo "----- A porta selecionada foi desativada com sucesso!  -----"
+				else
+				echo "Selecione uma das opção disponiveis. ssh | http | https "
+				fi
+				;;
+				
+				3)
+				echo "A sair do programa..."
+				break
+				exit 0
+				;;
+				
+				*)
+				echo "Opcção inválida! Selecione uma opção válida.
+				1: porta 22
+				2: porta 80
+				3: porta 443"
+				;;
+			esac
+		done
+			;;
+		2)
+		echo "----- A instalar PHP e os seus módulos necessários ------"
+		sudo dnf install php php-mysqlnd php-cli -y
+		echo "----- PHP e os seus módulos necessários instalados com sucesso ! -----"
+		;;
+		3)
+		echo "---- Criação dos arquivos -----"
+		echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/info.php
+		echo "---- A ajustar permissões -----"
+		sudo chown apache:apache /var/www/html/info.php
+		echo " ----- A reiniciar o apache para carregar o php -----"
+		sudo chown apache:apache /var/www/html/info.php
+		printf "/n"
+		cat << END
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!! PARA VER A INFO DO PHP ACEDA DESTE MODO:  !!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!https://<seu-ip>/info.php!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+END
+		;;
+		4)
+		echo "A fechar o programa.... "
+		exit 0
+	esac
+done 
