f5fa877ed484467ad18771096cb1e5b045d0741a
g-silva-005 <1999goncalo.gs@gmail.com>
2026-02-14 18:57:20 +0000

    Add files via upload

---
+++ seguranca.sh
@@ -0,0 +1,56 @@
+while true; do
+    clear
+    echo -e "\033[1;35m" # Roxo para destacar que é o Segurança
+    echo "  ██████╗███████╗███╗   ██╗████████╗ ██████╗ ███████╗"
+    echo " ██╔════╝██╔════╝████╗  ██║╚══██╔══╝██╔═══██╗██╔════╝"
+    echo " ██║     █████╗  ██╔██╗ ██║   ██║   ██║   ██║███████╗"
+    echo " ██║     ██╔══╝  ██║╚██╗██║   ██║   ██║   ██║╚════██║"
+    echo " ╚██████╗███████╗██║ ╚████║   ██║   ╚██████╔╝███████║"
+    echo "  ╚═════╝╚══════╝╚═╝  ╚═══╝   ╚═╝    ╚═════╝ ╚══════╝"
+    echo -e "\033[0m"
+    echo " [1] Atualizar o sistema   [3] Sincronizar hora"
+    echo " [2] Verificar Logs        [4] Configurar Backup Semanal"
+    echo " [Q] Terminar"
+    echo " ----------------------------------------------------"
+    read -p "[Admin]: " opt
+    case ${opt,,} in
+		1)
+			echo "---- A iniciar as Atualizações ----"
+			sleep 1
+			sudo dnf update -y
+			read -p "! Pressione ENTER para voltar ao menu !"
+			;;
+		2)
+			echo "---- Logs de acesso APACHE (a mostrar as ultimas 10 linhas) ----"
+			sudo tail -n 10 /var/log/httpd/access_log
+			echo ""
+			echo "---- Tentativas de login ----"
+			sudo tail -n 10 /var/log/secure
+			echo ""
+			read -p "! Pressione ENTER para voltar ao menu !"
+			;;
+		3)
+			echo "---- A sincronizar hora ----"
+			sudo systemctl restart chronyd
+		    sudo chronyc tracking
+			read -p "! Pressione ENTER para voltar ao menu !"
+			;;
+		4)
+            echo "---- A configurar Agendamento (Cron) ----"
+            SCRIPT_BACKUP="tar -czf /backup/www_$(date +\%F).tar.gz /var/www/html > /var/log/backup_exec.log 2>&1"     
+            (crontab -l 2>/dev/null; echo "00 03 * * 0 $SCRIPT_BACKUP") | crontab -
+			sleep 1
+			echo ""
+			echo "------------------------------------------------"
+			echo "Sucesso: Backup agendado para Domingo às 03:00!"
+			echo "Log de execução será guardado em: /var/log/backup_exec.log"
+			echo "------------------------------------------------"
+		;;
+		q|Q)
+			exit
+			;;
+		*)
+			;;
+	esac
+done 
+	
